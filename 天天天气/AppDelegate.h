17c22c2bd927da644d67d8a1d94fe44025e5f709
杨川 <yangchuan@yangchuandeAir.workgroup>
2015-07-30 21:52:30 +0800

    Initial Commit

---
+++ 天天天气/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  天天天气
+//
+//  Created by 杨川 on 15/7/30.
+//  Copyright (c) 2015年 杨川. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 天天天气/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  天天天气
+//
+//  Created by 杨川 on 15/7/30.
+//  Copyright (c) 2015年 杨川. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
